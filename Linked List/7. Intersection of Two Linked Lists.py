af1cb2715bb590293000ef1fa1b2e70a909f797a
prith46 <prithiviraj17a@gmail.com>
2024-06-03 16:48:05 +0530

    done with LL

--- Linked List/7. Intersection of Two Linked Lists.py
+++ Linked List/7. Intersection of Two Linked Lists.py
@@ -5,3 +5,53 @@ Sample Input: 8 -> 4 -> 5 (for list A), 5 -> 0 -> 1 -> 8 -> 4 -> 5 (for list B)
 Sample Output: 8 -> 4 -> 5
 """
 
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def getIntersectionNode(headA, headB):
+    if not headA or not headB:
+        return None
+
+    pointerA = headA
+    pointerB = headB
+    while pointerA is not pointerB:
+        if pointerA:
+            pointerA = pointerA.next
+        else:
+            pointerA = headB
+
+        if pointerB:
+            pointerB = pointerB.next
+        else:
+            pointerB = headA
+
+    return pointerA
+
+
+# Example usage:
+# Creating two linked lists with an intersection
+# List A: 4 -> 1 \
+#                8 -> 4 -> 5
+# List B:    5 -> 0 -> 1 /
+
+# Common part
+common = ListNode(8, ListNode(4, ListNode(5)))
+
+# List A
+headA = ListNode(4, ListNode(1, common))
+
+# List B
+headB = ListNode(5, ListNode(0, ListNode(1, common)))
+
+# Finding intersection
+intersection = getIntersectionNode(headA, headB)
+
+# Printing result
+if intersection:
+    print("Intersection at node with value:", intersection.val)
+else:
+    print("No intersection")
